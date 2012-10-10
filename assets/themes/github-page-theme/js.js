2d5c56c37f3c79247d68a6a9aca0a1a9abd0b561
tatsuki yokomine <z.ibra.veste@gmail.com>
2012-10-10 09:40:49 +0900

    logo color

---
+++ assets/themes/github-page-theme/js.js
@@ -0,0 +1,12 @@
+var count;
+function BackgroundChange(){
+  $(function(){
+    var color = Math.floor(Math.random() * 0xFFFFFF).toString(16);  //#RRGGBBを取得
+      for(count = color.length; count < 6; count++){
+            color = "0" + color;            //上位に0を補完する
+      }
+      color = "#" + color;                    //最上位に#を付ける
+      $('#logo').css('background-color',color);       //背景色変
+  });
+}
+window.onload = BackgroundChange;
